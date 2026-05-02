d1b029d21376a33c4f5ec7e68f9f1376bf780c4c
khushigpt <khushi.gptk210@gmail.com>
2026-05-02 15:56:32 +0530

    Add health check endpoint to app.js

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const server = http.createServer((req, res) => {
     res.write("Hello from CI/CD Pipeline 🚀- Version 2");
     res.end();
 });
+app.get('/', (req, res) => {
+    res.send("Hello from CI/CD Pipeline 🚀 - v2");
+});
 
+app.get('/health', (req, res) => {
+    res.status(200).send("OK");
+});
 server.listen(3000, () => {
     console.log("Server running on port 3000");
 });
